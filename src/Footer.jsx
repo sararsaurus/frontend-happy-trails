720e0d985f7acff2252e25e791bfe3c08c8aed45
Sara Raley <sara.m.raley@gmail.com>
2023-01-03 12:48:22 -0700

    installs bootstrap

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <div className="container text-center">
+      <div className="row">
+        <div className="col">© 90's trust fund authentic woke!</div>
+      </div>
+    </div>
+  );
+}
