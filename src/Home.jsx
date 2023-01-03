5de56ac1024d7d91afb46473483f3614adec9c9a
Sara Raley <sara.m.raley@gmail.com>
2023-01-03 14:17:01 -0700

    adds loop to get fast facts for each trail

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,28 +3,9 @@ import { useState, useEffect } from "react";
 import { TrailsIndex } from "./TrailsIndex";
 
 export function Home() {
-  // const trails = [
-  //   {
-  //     id: 1,
-  //     name: "best trail",
-  //     description: "so good",
-  //     length: 1,
-  //     difficulty: "easy",
-  //     image_url: "https://i.imgur.com/j4fcHza_d.jpg?maxwidth=520&shape=thumb&fidelity=high",
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "second best trail",
-  //     description: "not quite as good",
-  //     length: 2,
-  //     difficulty: "easy",
-  //     image_url: "https://i.imgur.com/bVxmfdx_d.jpg?maxwidth=520&shape=thumb&fidelity=high",
-  //   },
-  // ];
   const [trails, setTrails] = useState([]);
 
   const handleIndexTrails = () => {
-    console.log("handleIndexTrails");
     axios.get("http://localhost:3000/trails.json").then((response) => {
       console.log(response.data);
       setTrails(response.data);
--- src/TrailsIndex.jsx
+++ src/TrailsIndex.jsx
@@ -9,19 +9,28 @@ export function TrailsIndex(props) {
           <p>Description: {trail.description}</p>
           <p>Length: {trail.length}</p>
           <p>Difficulty: {trail.difficulty}</p>
-          <h3>
-            Conditions:{" "}
-            {trail.conditions.map((condition) => (
-              <div key={condition.id}>
-                <p>Trail condition: {condition.trail_condition}</p>
-                <p>Temp: {condition.temp}</p>
-                <p>Forecast: {condition.forecast}</p>
-                <p>Id: {condition.trail_id}</p>
-                <p>Sunrise: {condition.sunrise_time}</p>
-                <p>Sunset: {condition.sunset_time}</p>
-              </div>
-            ))}
-          </h3>
+          {trail.conditions.map((condition) => (
+            <div key={condition.id}>
+              <h3>Conditions:</h3>
+              <p>Trail condition: {condition.trail_condition}</p>
+              <p>Temp: {condition.temp}</p>
+              <p>Forecast: {condition.forecast}</p>
+              <p>Trail id: {condition.trail_id}</p>
+              <p>Sunrise: {condition.sunrise_time}</p>
+              <p>Sunset: {condition.sunset_time}</p>
+            </div>
+          ))}
+          {trail.fast_facts.map((fast_fact) => (
+            <div key={fast_fact.id}>
+              <h3>Fast Facts:</h3>
+              <p>General: {fast_fact.general} </p>
+              <p>Nearby_landmarks: {fast_fact.nearby_landmarks} </p>
+              <p>Lake_info: {fast_fact.lake_info} </p>
+              <p>Backcountry_zones: {fast_fact.backcountry_zones} </p>
+              <p>Ecological_zones: {fast_fact.ecological_zones} </p>
+              <p>Trail_id: {fast_fact.trail_id} </p>
+            </div>
+          ))}
         </div>
       ))}
     </div>
