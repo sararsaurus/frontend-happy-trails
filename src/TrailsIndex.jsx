075590d80549d0cb71335c5d4dad610b4457d468
Sara Raley <sara.m.raley@gmail.com>
2023-01-03 13:17:08 -0700

    starts working on web hooks

--- src/TrailsIndex.jsx
+++ src/TrailsIndex.jsx
@@ -1,7 +1,16 @@
-export function TrailsIndex() {
+export function TrailsIndex(props) {
   return (
     <div>
       <h1>All TRAILS (but not)</h1>
+      {props.trails.map((trail) => (
+        <div key={trail.id}>
+          <h2>{trail.name}</h2>
+          <img src={trail.image_url} alt="trail photo" />
+          <p>Description: {trail.description}</p>
+          <p>Length: {trail.length}</p>
+          <p>Difficulty: {trail.difficulty}</p>
+        </div>
+      ))}
     </div>
   );
 }
