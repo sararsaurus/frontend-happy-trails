dd2a8e0ad707e39254a1f709531e1dbb013b68f1
Sara Raley <sara.m.raley@gmail.com>
2023-01-03 20:25:00 -0700

    makes scheduled hikes a little cute

--- src/HikeSchedulesIndex.jsx
+++ src/HikeSchedulesIndex.jsx
@@ -1,14 +1,21 @@
 export function HikeSchedulesIndex(props) {
   return (
-    <div>
-      <h1>Your Hike Schedule</h1>
-      {props.hikes.map((hike) => (
-        <div key={hike.id}>
-          <h4>Date: {hike.date}</h4>
-          <p>Trail: {hike.trail_name}</p>
-          <button onClick={() => props.onShowHike(hike)}>More info</button>
-        </div>
-      ))}
+    <div id="hikes-index">
+      <h1 className="text-center">Your Scheduled Hikes</h1>
+      <br />
+      <div className="row">
+        {props.hikes.map((hike) => (
+          <div className="col-4" key={hike.id}>
+            <h4>{hike.date}</h4>
+            <p>{hike.trail_name}</p>
+            <button onClick={() => props.onShowHike(hike)}>More info</button>
+          </div>
+        ))}
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
