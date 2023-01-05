d87a9bf320a31d2a5b86b44b7c154aaac0347dfd
Sara Raley <sara.m.raley@gmail.com>
2023-01-04 18:24:56 -0700

    fixes jwt conditional for scheduled hikes index

--- src/HikeSchedulesIndex.jsx
+++ src/HikeSchedulesIndex.jsx
@@ -1,18 +1,21 @@
 export function HikesIndex(props) {
-  return (
-    <div id="hikes-index">
-      <h1 className="text-center">Your Scheduled Hikes</h1>
-      <br />
-      <div className="row">
-        {props.hikes.map((hike) => (
-          <div className="col-4" key={hike.id}>
-            <h4>{hike.date}</h4>
-            <p>{hike.trail_name}</p>
-            <button onClick={() => props.onShowHike(hike)}>More info</button>
-          </div>
-        ))}
+  const jwt = localStorage.getItem("jwt");
+  if (jwt) {
+    return (
+      <div id="hikes-index">
+        <h1 className="text-center">Your Scheduled Hikes</h1>
+        <br />
+        <div className="row">
+          {props.hikes.map((hike) => (
+            <div className="col-4" key={hike.id}>
+              <h4>{hike.date}</h4>
+              <p>{hike.trail_name}</p>
+              <button onClick={() => props.onShowHike(hike)}>More info</button>
+            </div>
+          ))}
+        </div>
+        <br />
       </div>
-      <br />
-    </div>
-  );
+    );
+  }
 }
